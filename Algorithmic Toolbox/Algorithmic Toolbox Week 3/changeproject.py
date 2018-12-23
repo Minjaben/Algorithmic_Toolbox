ebaebbfa9def0033e5b7d0b2ede3fa0f5da185e8
Minjaben <minjaben@gmail.com>
2018-12-23 11:58:30 +0900

    Updating the changeproject.py file to the working version.

--- Algorithmic Toolbox/Algorithmic Toolbox Week 3/changeproject.py
+++ Algorithmic Toolbox/Algorithmic Toolbox Week 3/changeproject.py
@@ -2,11 +2,21 @@
 import sys
 def get_change(m):
     #For testing purposes:
-    print (m + "is executing the function.")
-    
-    return m
+    coincount = 0
+   # print (m + "is executing the function.")
+    while m > 0:
+        if m >= 10:
+            m -= 10
+            coincount += 1
+        elif m >= 5:
+            m -= 5
+            coincount += 1
+        else:
+            m -= 1
+            coincount += 1
+    return coincount
 
 
 m = int(input())
-print (str(m))
+#print (m)
 print(get_change(m))
